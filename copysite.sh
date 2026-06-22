9524618173805fd5fef71477030dc52550e738ac
Carl Sampson <carl.sampson@gmail.com>
2026-06-22 15:41:28 -0400

    changes

--- copysite.sh
+++ copysite.sh
@@ -16,3 +16,20 @@ rsync -avz --delete -e ssh \
   --exclude 'backups' \
   /Users/chs/domains/dd4sb/ chs@chs.us:/home/chs/denisedye4noblesville.com
 
+# Notify IndexNow (Bing, Yandex, DuckDuckGo, etc.) that content changed.
+# Key file is served at https://denisedye4noblesville.com/<KEY>.txt
+INDEXNOW_KEY="cffcc1aaa55528a2649e5ba23d47448c"
+curl -s -X POST "https://api.indexnow.org/indexnow" \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -d '{
+    "host": "denisedye4noblesville.com",
+    "key": "'"$INDEXNOW_KEY"'",
+    "keyLocation": "https://denisedye4noblesville.com/'"$INDEXNOW_KEY"'.txt",
+    "urlList": [
+      "https://denisedye4noblesville.com/",
+      "https://denisedye4noblesville.com/denise.html",
+      "https://denisedye4noblesville.com/forms/volunteer.html",
+      "https://denisedye4noblesville.com/forms/contact.html"
+    ]
+  }' -o /dev/null -w "IndexNow: HTTP %{http_code}\n" || true
+
